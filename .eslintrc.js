81b6e1b72e4b09c9179642eadb2e1d5f7ebc57c3
Phillip Rearick <psrearick@gmail.com>
2023-11-03 14:29:06 -0400

    wip

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
--- src/js/loadFonts.js
+++ src/js/loadFonts.js
@@ -4,10 +4,10 @@ const MontserratObserver = new FontFaceObserver('Montserrat');
 const InterObserver = new FontFaceObserver('Inter');
 
 Promise.all([MontserratObserver.load(), InterObserver.load()]).then(() => {
-    document
-        .querySelectorAll('.font-sans')
-        .forEach((el) => el.classList.add('fonts-loaded'));
-    document
-        .querySelectorAll('.font-serif')
-        .forEach((el) => el.classList.add('fonts-loaded'));
+  document
+    .querySelectorAll('.font-sans')
+    .forEach((el) => el.classList.add('fonts-loaded'));
+  document
+    .querySelectorAll('.font-serif')
+    .forEach((el) => el.classList.add('fonts-loaded'));
 });
